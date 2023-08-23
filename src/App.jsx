b64aa58535e4aeba83fcf5b49e3484eaf9b576aa
Vachagan Nersesyan <vach.nersesyan@bk.ru>
2023-08-23 21:32:03 +0400

    my comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import HeaderFuncComponent from './components/HeaderComp/HeaderScp'
+import SearchFuncComponent from './components/SearchComp/SearchScp'
+import TodoListFuncComponent from './components/TodoListComp/TodoListScp'
+
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Search />
-        <TodoList />
+        <HeaderFuncComponent />
+        <SearchFuncComponent />
+        <TodoListFuncComponent />
     </div>
   );
 }
